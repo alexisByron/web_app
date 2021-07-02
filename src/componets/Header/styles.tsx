3e7ba8f84b4062132bc7102c5ecd3991b85b976a
AlexisMoya98 <ext_amoya@sbpay.cl>
2021-07-01 22:21:01 -0400

    [ALEXIS] cambio style header

--- src/componets/Header/styles.tsx
+++ src/componets/Header/styles.tsx
@@ -6,6 +6,9 @@ const header: CSS.Properties = {
   top: 0,
   right: 0,
   textAlign: "center",
+  backgroundImage:`url('https://i.pinimg.com/236x/c3/57/82/c357826c8510cdfb78439343fefcac5d--seamless-background-vector-background.jpg')`,
+  borderBottomLeftRadius:'40%',
+  borderBottomRightRadius:'40%'
 };
 
 export const styles = {
