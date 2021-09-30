795675a4a89b7ad3e8758aeb7d4fb29bf1c3389c
AlexisMoya98 <ext_amoya@sbpay.cl>
2021-09-30 16:42:22 -0300

    ultimo refactor para deploy

--- src/componets/CustomList/CustomList.tsx
+++ src/componets/CustomList/CustomList.tsx
@@ -4,39 +4,37 @@ import { Product } from "../../componets/Product/Product";
 import { PaginatedList } from "react-paginated-list";
 
 export const CustomList = ({ list }: customListProps) => {
-  const RenderList = () => {
-    return list.map((element: any, id) => {
-      return (
-        <Product
-          key={id}
-          id={element.idApartment}
-          imagen={element.imagenPrincipal}
-          nombreProducto={element.title}
-          bathRooms={element.bathRoom}
-          bedRooms={element.bedRooms}
-          comuna={element.comuna}
-        />
-      );
-    });
-  };
-
   return (
-    <PaginatedList
-      list={list}
-      itemsPerPage={12}
-      renderList={(list) => (
-        <div
-          className="container-c grid-items"
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(4, calc(25% - 22.5px))",
-            gridColumnGap: "30px",
-            gridRowGap: "30px",
-          }}
-        >
-          {RenderList()}
-        </div>
-      )}
-    />
+    <>
+      <PaginatedList
+        list={list}
+        itemsPerPage={8}
+        renderList={(list) => (
+          <div
+            className="container-c grid-items"
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(4, calc(25% - 22.5px))",
+              gridColumnGap: "30px",
+              gridRowGap: "30px",
+            }}
+          >
+            {list.map((item, id) => {
+              return (
+                <Product
+                  key={id}
+                  id={item.idApartment}
+                  imagen={item.imagenPrincipal}
+                  nombreProducto={item.title}
+                  bathRooms={item.bathRoom}
+                  bedRooms={item.bedRooms}
+                  comuna={item.comuna}
+                />
+              );
+            })}
+          </div>
+        )}
+      />
+    </>
   );
 };
